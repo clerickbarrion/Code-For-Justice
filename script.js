c7aae36bc34228afcd9dc36b38e98db056f4ab69
clerickbarrion <clerickbarrion@gmail.com>
2023-10-12 09:23:13 -0400

    finished calendar form validation

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ const removeState = () => {
     country.toUpperCase() != 'UNITED STATES' ?  state.setAttribute('disabled', '') : state.removeAttribute('disabled')
 }
 
-let currentDate = new Date()
-console.log(currentDate)
-currentDate.setMonth(currentDate.getMonth() + 1);
-console.log(currentDate)
+let nextMonth = new Date()
+
+nextMonth.setMonth(nextMonth.getMonth() + 1);
+
 
 const checkDate = () => {
     date = document.getElementById('date');
-    console.log(date.value)
-    console.log(date.value.getTime() >= currentDate.getTime())
-    if (date.value.getTime() >= currentDate.getTime()) {
-        date.setAttribute('isvalid','true')
+    let year = date.value.slice(0,4)
+    let month = date.value.slice(5,7)
+    let day = date.value.slice(8,10)
+    let hour = date.value.slice(11,13)
+    let minute = date.value.slice(14,17)
+    nextDate = new Date(`${year}-${month}-${day}T${hour}:${minute}`)
+    console.log(nextDate >= nextMonth)
+    if (nextDate >= nextMonth) {
+        date.setCustomValidity('')
+        date.validity.valid = true
+        date.validity.customError = false
     } else {
-        date.setCustomValidity('Enter at least 1 month in advance from today');
+        date.validity.valid = false
+        date.setCustomValidity('Enter at least 1 month in advance from today')
     }
 }
